6f76069b581ce4c736090683a130bff0aedb5d3e
Hélio Costa <tweenzx@gmail.com>
2023-10-01 16:49:04 -0300

    feat: add example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
-
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import SkeletonImage from 'react-native-skeleton-image';
 
 export default function App() {
+  const uri = `https://placehold.co/300x200/palegreen/azure.png?text=Hello,%20world!&v=${new Date().getTime()}`;
+
   return (
     <View style={styles.container}>
-      <></>
+      <Text>{"RN's <Image />"}</Text>
+      <Image style={styles.image} source={{ uri }} />
+
+      <View style={styles.spacer} />
+
+      <Text>{'<SkeletonImage />'}</Text>
+      <SkeletonImage style={styles.image} source={{ uri }} />
     </View>
   );
 }
@@ -15,5 +23,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  spacer: {
+    height: 24,
+  },
+  image: {
+    width: 300,
+    height: 200,
+    borderRadius: 8,
   },
 });
